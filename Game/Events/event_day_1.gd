62d74293c2cce4d5bb47672c0a3900fa9eeefaa7
Gabriel Smereka <farofafactionoficial@gmail.com>
2024-10-08 20:15:32 -0300

    add-knock-knock

--- Game/Events/event_day_1.gd
+++ Game/Events/event_day_1.gd
@@ -8,7 +8,14 @@ func _ready() -> void:
 	GlobalTimer.set_speed(6)
 	pass # Replace with function body.
 
-
+var event_1 := false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if GlobalTimer.hours < 18:
+		return
+	if GlobalTimer.minutes < 0:
+		return
+	if (!$DoctorKnocking.playing):
+		$DoctorKnocking.play()
+		#event_1 = true
 	pass
