49961fbce98654f3b467046abcc14f7762884220
FarofaFaction <farofafactionoficial@gmail.com>
2024-10-15 14:36:37 -0300

    update-game

---
+++ Game/Loctions/PlayerRoom/enemy_spawning.gd
@@ -0,0 +1,31 @@
+extends Node
+
+@export var possible_positions : Array[Marker2D]
+@export var spawn_node : Node
+@export var spawn_times : Array[Vector2] = [Vector2(20, 0), Vector2(20, 30)]
+var enemyScene = preload("res://Game/Enemy/red_guy.tscn")
+var spawned_times : Array = []
+
+# Called when the enemy enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if !spawn_node:
+		return
+	
+	for time in spawn_times:
+		if not time in spawned_times:
+			if GlobalTimer.hours >= time.x && GlobalTimer.minutes >= time.y:
+				var enemy = enemyScene.instantiate()
+				if possible_positions.size() > 0:
+					var random_index = randi() % possible_positions.size()
+					var random_position = possible_positions[random_index].global_position
+					enemy.global_position = random_position
+				else:
+					enemy.global_position = Global.player.global_position
+					enemy.global_position.x += 100
+				spawn_node.add_child(enemy)
+				spawned_times.append(time)  # Track that this time has been used
+				break  # Exit the loop after spawning to prevent multiple spawns in one frame
--- Game/Loctions/PlayerRoom/player_room.gd
+++ Game/Loctions/PlayerRoom/player_room.gd
@@ -12,16 +12,16 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-var _spawned := false
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if _spawned:
-		return
-		
-	if GlobalTimer.hours >= 6:
-		var node = preload("res://Game/Enemy/red_guy.tscn").instantiate()
-		node.global_position = Global.player.global_position
-		node.global_position.x += 100
-		$OrganizadosPorY/Personagens.add_child(node)
-		_spawned = true
-	pass
+#var _spawned := false
+### Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(_delta: float) -> void:
+	#if _spawned:
+		#return
+		#
+	#if GlobalTimer.hours >= 6:
+		#var node = preload("res://Game/Enemy/red_guy.tscn").instantiate()
+		#node.global_position = Global.player.global_position
+		#node.global_position.x += 100
+		#$OrganizadosPorY/Personagens.add_child(node)
+		#_spawned = true
+	#pass
